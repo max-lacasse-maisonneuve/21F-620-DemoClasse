9c81d1e75ffab13b2c30322a4293549b494f4bdd
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-06-28 15:04:35 -0400

    Ajout cours

--- 02_Demo/assets/js/main.js
+++ 02_Demo/assets/js/main.js
@@ -1,16 +1,82 @@
 // Variables
 
 const chemin = ["assets", "img", "albums"];
-const albums = ["Mezzanine", "Back in Black", "The Dark Side of the Moon", "Abbey Road", "Kind of Blue", "The Wall"];
+const albums = [
+    "Mezzanine",
+    "Back in Black",
+    "   The Dark Side of the Moon  ",
+    "Abbey Road",
+    "Kind of Blue",
+    "The Wall",
+];
 
 // const template = `
 // <div class="album" id="">
 //     <img src="assets/img/albums/doors_the_doors.webp" alt="" />
 // </div>`;
 
-// Créer une fonction qui formatte le chemin des images
-// Créer une fonction qui génère le template HTML et l'injecte dans le DOM
-// Créer une fonction qui affiche les albums au chargement de la page
+const conteneurAlbumHTML = document.querySelector(".liste-albums");
 
-// Au clic du bouton, vider le contenu du conteneur et réafficher les albums
-// Au clic d'une div album, afficher le id dans la console et afficher l'attribut alt de l'image dans la console
+//Fonction qui est appelée au chargement du fichier
+// Créer une fonction qui affiche les albums au chargement de la page
+function init() {
+    albums.forEach(function (album, index) {
+        let albumHTML = ajouterAlbumHTML(album);
+        console.log(albumHTML);
+    });
+}
+
+// Créer une fonction qui formatte le chemin des images
+function formatterChemin(tableauChemin) {
+    let chemin = "";
+    //Joindre les éléments du tableau
+    chemin = `${tableauChemin.join("/")}/`; //Join crée une chaine de caractère
+
+    //Retourner une chaine avec la base du chemin
+    return chemin;
+}
+
+//Créer une fonction qui formatte la source de l'image d'un album
+function formatterNomAlbum(nomAlbum) {
+    //Récupérer la base des images
+    let base = formatterChemin(chemin);
+
+    //Nettoyer les espaces avant ou arrières
+    let nomNettoye = nomAlbum.trim();
+
+    //Mettre en minuscule
+    nomNettoye = nomNettoye.toLowerCase();
+    //Remplacer les espaces par des _
+
+    nomNettoye = nomNettoye.replaceAll(" ", "_");
+    //Retourner la source de l'image. Ex: assets/img/albums/doors_the_doors.webp
+    let sourceImage = `${base}${nomNettoye}.webp`;
+
+    return sourceImage;
+}
+
+// Créer une fonction qui génère le template HTML et l'injecte dans le DOM
+function ajouterAlbumHTML(nomAlbum) {
+    let sourceImage = formatterNomAlbum(nomAlbum);
+    const template = `
+        <div class="album" id="">
+            <img src="${sourceImage}" alt="Image pour l'album ${nomAlbum}" />
+        </div>`;
+
+    conteneurAlbumHTML.insertAdjacentHTML("beforeend", template);
+
+    let elementHTML = conteneurAlbumHTML.lastElementChild;
+    elementHTML.addEventListener("click", function () {
+        elementHTML.style.border = "2px solid tomato";
+    });
+    // Au clic d'une div album, afficher le id dans la console et afficher l'attribut alt de l'image dans la console
+    return elementHTML;
+}
+
+const img = document.querySelector("img");
+img.src = "sldf/sdfsdf/sdf.webp"
+let src = img.src
+// Au clic du bouton, vider le contenu du conteneur, trier et réafficher les albums
+
+//Exécution
+init();
--- 03_Exercice/assets/js/main.js
+++ 03_Exercice/assets/js/main.js
@@ -1,4 +1,52 @@
-// N'oubliez pas de structurer votre code correctement. Voir cours 3
+// Variables
 const liensPiedPage = ["  Mentions légales", " Politique de confidentialité   ", " Plan du site   "];
 
-// Variables
+//Sélections HTML
+const piedPageHTML = document.querySelector(".listes-liens-secondaires ul");
+
+//Fonction appelée au chargement de la page
+function init() {
+    liensPiedPage.forEach(function (lien) {
+        console.log(lien);
+        injecterPiedPage(lien);
+    });
+}
+
+//Fonction de formattage des liens
+function formatterLien(lien) {
+    let lienFormatte = lien.trim(); //Nettoie le texte
+    lienFormatte = lienFormatte.toLowerCase(); //Met en minuscule
+
+    //Défi 1: Enlève les accents
+    lienFormatte = lienFormatte.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+    lienFormatte = lienFormatte.replaceAll(" ", "-"); //Remplace les espaces par des tirets
+    lienFormatte = `${lienFormatte}.html`; //Ajoute l'extension du lien
+
+    return lienFormatte;
+}
+
+//Fonction servant à injecter le lien sur la page
+function injecterPiedPage(lien) {
+    //On formatte le lien
+    let lienFormatte = formatterLien(lien);
+
+    //Le gabarit à injecter
+    let gabarit = `
+        <li>
+            <a href="${lienFormatte}"/>${lien}</a>
+        </li>`;
+
+    //On ajoute le lien sur la page
+    piedPageHTML.insertAdjacentHTML("beforeend", gabarit);
+
+    let lienAjoute = piedPageHTML.lastElementChild;
+
+    //Défi 2: afficher l'attribut href du lien en survol
+    lienAjoute.addEventListener("mouseenter", function () {
+        let href = lienAjoute.querySelector("a").href;
+        console.log(href);
+    });
+}
+
+init();
