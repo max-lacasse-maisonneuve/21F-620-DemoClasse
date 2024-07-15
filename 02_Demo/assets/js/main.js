d5f901c05f9b82ce590a920958c7cb14de77c14f
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-07-15 14:21:10 -0400

    Ajout cours 10

--- 02_Demo/assets/js/main.js
+++ 02_Demo/assets/js/main.js
@@ -1,88 +1,25 @@
-// // const navigation = document.querySelector("#nav-principale");
-// const navigation = document.getElementById("nav-principale");
+// Variables
+let sectionActuelle = 0;
 
-// const li = navigation.querySelector("li");
+// Éléments HTML
+// - Formulaire
+// - Sections
+// - Boutons
 
-// const enfantsNavigation = navigation.children;
+//Fonctions
+function init() {}
 
-// const premierEnfantNavListe = enfantsNavigation[1].firstElementChild;
-// let navListe = enfantsNavigation[1];
-// // console.log(navListe);
+function onSubmit() {}
 
-// for (let i = 0; i < navListe.children.length; i++) {
-//     console.log(navListe.children[i]);
-// }
-// const mix = document.querySelectorAll("[href],h1,section");
+//Désactiver le bouton précédent si on est sur la première section
+//Désactiver le bouton suivant si on est sur la dernière section
+function afficherSection() {}
 
-// const button = document.querySelector('[type="button"]');
-// console.log(button);
-// const parentBouton = button.closest("main");
-// console.log(parentBouton);
+function toutCacher() {}
 
-//Méthode 1
-// let gabarit1 = `<div>allo</div>`;
-// document.querySelector(".panier-achat").insertAdjacentHTML("beforeend", gabarit);
-// let element = document.querySelector(".panier-achat").lastElementChild;
-// element.addEventListener("click", function () {
-//     console.log("click");
-// });
+function avancerSection() {}
 
-//Méthode 2
-// let gabarit = document.querySelector("template");
-// let clone = gabarit.content.cloneNode(true);
-// clone.querySelector("h1").textContent = "Nouveau texte";
-// document.querySelector(".panier-achat").append(clone);
+function reculerSection() {}
 
-// clone.addEventListener("click", function () {
-//     console.log("click");
-// });
-
-//Méthode 3 = Dernier recours
-// let div = document.createElement("div");
-// let h1 = document.createElement("h1");
-// h1.textContent = "nouveau texte";
-// div.append(h1);
-// document.querySelector(".panier-achat").append(div);
-
-// document.querySelector(".panier-achat").lastElementChild.remove();
-
-//Sélectionner avec attribut, id,classe
-//Sélectionner avec ID
-
-// Modifier la classe d'un élément, classList
-// Modifier les attributs d'un élément. Href, src, alt, id.
-// Attribut dataset
-
-//=================================================================
-//Fonction pour cloner un élément du panier achat
-//Au double-clic, ajouter l'élément au panier achat
-//Sélectionner l'élément et supprimer au survol
-
-//==== Éléments HTML
-const panierAchat = document.querySelector(".panier-achat");
-const gabarit = document.querySelector("template");
-const button = document.getElementById("bouton-ajout");
-
-//=== Fonctions
-function initialiser() {
-    button.addEventListener("click", clonerElement);
-    let boutonInput = document.querySelector("input");
-    boutonInput.value = "Nouveau texte";
-}
-
-function clonerElement() {
-    let clone = gabarit.content.cloneNode(true);
-    panierAchat.append(clone);
-    let element = panierAchat.lastElementChild;
-
-    element.addEventListener("mouseenter", function () {
-        // element.remove();
-        element.classList.add("selection");
-    });
-
-    element.addEventListener("mouseleave", function () {
-        element.classList.remove("selection");
-    });
-}
-
-initialiser();
+// Exécution
+init();
--- 03_Exercice/assets/js/main.js
+++ 03_Exercice/assets/js/main.js
@@ -1,64 +1,9 @@
 // Variables
 
 // Éléments HTML
-const gabarit = document.querySelector("template");
-const champNombre = document.querySelector('[type="number"]');
-const sectionResume = document.querySelector(".resume");
 
 // Fonctions
-/**
- * Fonction appelée au chargement de la page
- */
-function initialiser() {
-    //On ajoute un écouteur d'événement sur le champ de nombre
-    //Lorsque la valeur change, on appelle la fonction changement
-    champNombre.addEventListener("change", changement);
+function initialiser() {}
 
-    //On crée un nombre d'éléments égal à la valeur du champ au chargement de la page
-    for (let i = 0; i < champNombre.value; i++) {
-        cloner(i);
-    }
-}
-
-/**
- * Fonction appelée lorsqu'on change la valeur du champ de nombre
- * On vide la section de résumé
- * On crée un nombre d'éléments égal à la valeur du champ
- */
-function changement() {
-    const valeur = champNombre.value; //On récupère la valeur du champ
-    sectionResume.innerHTML = ""; //On vide la section de résumé
-
-    //On crée un nombre d'éléments égal à la valeur du champ
-    for (let i = 0; i < valeur; i++) {
-        cloner(i);
-    }
-}
-
-/**
- * Fonction pour cloner un élément du panier achat
- * Au clic sur le bouton d'une boite, on supprime et on met à jour le nombre de boites dans le champ
- * @param {Number} index
- */
-function cloner(index) {
-    //On clone le gabarit
-    //true permet de cloner les enfants
-    let clone = gabarit.content.cloneNode(true);
-    sectionResume.append(clone); // On ajoute le clone à la section de résumé
-
-    //On récupère le dernier élément ajouté
-    let element = sectionResume.lastElementChild;
-
-    //On ajoute un écouteur d'événement sur le bouton de la boite
-    let bouton = element.querySelector(".bouton");
-    bouton.addEventListener("click", function () {
-        //On supprime l'élément
-        element.remove();
-
-        //On met à jour le nombre de boites dans le champ
-        let nombreRestant = sectionResume.children.length;
-        champNombre.value = nombreRestant;
-    });
-}
 // Exécution
 initialiser();
