06928c5ac885ed218b1a08d96ff680a33456de3c
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-07-15 07:44:49 -0400

    Ajout

--- 02_Demo/assets/js/main.js
+++ 02_Demo/assets/js/main.js
@@ -3,6 +3,10 @@ const panierAchat = document.querySelector(".panier-achat");
 //Sélectionner avec attribut, id,classe
 //Sélectionner avec ID
 
+// Modifier la classe d'un élément, classList
+// Modifier les attributs d'un élément. Href, src, alt, id.
+// Attribut dataset
+
 //Fonction pour cloner un élément du panier achat
 //Au double-clic, ajouter l'élément au panier achat
 //Sélectionner le parent et supprimer au survol
