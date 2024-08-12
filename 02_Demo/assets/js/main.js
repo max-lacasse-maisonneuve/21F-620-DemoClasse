3a576ba619faf7936b9ebad58457c4b8bc385d0b
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-08-12 12:52:52 -0400

    Ajout cours 10

--- 02_Demo/assets/js/main.js
+++ 02_Demo/assets/js/main.js
@@ -7,17 +7,24 @@ const formulaire = document.querySelector("#formulaire-principal");
 const champs = formulaire.querySelectorAll("input, textarea,select");
 const sections = formulaire.querySelectorAll("section[data-page]");
 
+// - Boutons
+const boutonsAvancer = formulaire.querySelectorAll("[data-direction='1']");
+const boutonsReculer = formulaire.querySelectorAll("[data-direction='-1']");
+
 // TODO: Voir CSS pour cacher les messages d'erreurs
 
 //Fonctions
 function init() {
     formulaire.addEventListener("submit", onSubmit);
+    //TODO: Écouteurs événement pour les boutons de navigation
 
     champs.forEach(function (element) {
         element.addEventListener("change", onChangementChamp);
     });
 
     // Valider la première section au chargement de la page
+
+    //TODO: Afficher la première section au chargement et cacher les autres
 }
 
 function onSubmit(evenement) {
@@ -86,20 +93,49 @@ function validerFormulaire() {
 }
 
 function afficherResumeCheckbox(nomChamp, estCoche) {
-  const champResume = sectionResume.querySelector(`[data-name="${nomChamp}"]`);
-  // console.log(champResume);
-  if (champResume !== null) {
-      let texte = estCoche ? "OUI" : "NON";
-      champResume.textContent = texte;
-  }
+    const champResume = sectionResume.querySelector(`[data-name="${nomChamp}"]`);
+    // console.log(champResume);
+    if (champResume !== null) {
+        let texte = estCoche ? "OUI" : "NON";
+        champResume.textContent = texte;
+    }
 }
 
 function afficherResume(nomChamp, valeur) {
-  const champResume = sectionResume.querySelector(`[data-name="${nomChamp}"]`);
-  // console.log(champResume);
-  if (champResume !== null) {
-      champResume.textContent = valeur;
-  }
+    const champResume = sectionResume.querySelector(`[data-name="${nomChamp}"]`);
+    // console.log(champResume);
+    if (champResume !== null) {
+        champResume.textContent = valeur;
+    }
 }
+
+//=============================================
+// AFFICHAGE et NAVIGATION Cours 10
+//=============================================
+
+function afficherSection() {
+    toutCacher();
+    sections[sectionActuelle].classList.remove("invisible");
+    //TODO: Désactiver le bouton précédent si on est sur la première section
+    //TODO: Désactiver le bouton suivant si on est sur la dernière section
+}
+
+function toutCacher() {
+    sections.forEach(function (element) {
+        element.classList.add("invisible");
+    });
+}
+
+function avancerSection(evenement) {
+    const declencheur = evenement.currentTarget;
+    sectionActuelle++;
+
+    if (sectionActuelle < sections.length) {
+        afficherSection();
+    }
+}
+
+//TODO: Fonction pour reculer à la section précédente
+
 // Exécution
 init();
