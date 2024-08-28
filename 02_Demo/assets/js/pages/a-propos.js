116b667b853e1ecb89080c8ecd7137f36824aacd
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-08-27 21:16:45 -0400

    TODO

--- 02_Demo/assets/js/pages/a-propos.js
+++ 02_Demo/assets/js/pages/a-propos.js
@@ -1,5 +1,12 @@
+//TODO: Séparation des données: modules, classes, import CSS, balise template dans HTML
+//TODO: Importer la navigation
+//TODO: Importer le code pour la boite modale
+//TODO: Expliquer comment faire une boite modale.
+//TODO: Au clic de fermeture, modifier le localStorage
+//TODO: Au chargement vérifier le localStorage
+//TODO: Expliquer le localStorage dans les outils du navigateur
+// TODO: Placer une IIFE dans le code principal
 
-function init() {
-}
+function init() {}
 
 init();
