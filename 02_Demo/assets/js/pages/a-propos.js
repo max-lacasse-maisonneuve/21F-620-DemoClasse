b2dad2de2867d8d6fad3719f7b5ce060fe275883
max-lacasse-maisonneuve <mlacassegermain@cmaisonneuve.qc.ca>
2024-09-17 15:03:42 -0400

    fin animation

--- 02_Demo/assets/js/pages/a-propos.js
+++ 02_Demo/assets/js/pages/a-propos.js
@@ -41,11 +41,59 @@ function init() {
     let cibles = document.querySelectorAll(".section");
     new ScrollAnimator(zone, cibles);
 
+    let soustitres = document.querySelectorAll("h2");
+
+    soustitres.forEach(function (soustitre, index) {
+        let animation;
+        soustitre.addEventListener("mouseenter", function () {
+            console.log(animation);
+
+            if (animation == undefined || animation.playState !== "running") {
+                let clesAnimation = [
+                    { color: "blue", opacity: 0.5, transform: "scale(1)" },
+                    { color: "red", opacity: 1, transform: "scale(2)" },
+                    { color: "limegreen", opacity: 0.75, transform: "scale(1)" },
+                ];
+                let options = {
+                    duration: 5000,
+                    fill: "forwards",
+                    easing: "ease-in-out",
+                    delay: 1000 * index,
+                };
+                animation = soustitre.animate(clesAnimation, options);
+
+                animation.addEventListener("finish", function () {
+                    console.log("terminé");
+                    // animation.play();
+                    // soustitre.remove();
+                });
+            }
+        });
+    });
+
+    // setInterval(function () {
+    //     console.log("patate");
+    // }, 1000);
+    compteur = performance.now();
+    requestAnimationFrame(testAnimation);
+
     // document.addEventListener("keydown", function (event) {
     //     console.log(event);
     // });
 }
 
+function testAnimation() {
+    let tempsActuel = performance.now();
+    let tempsEcoule = tempsActuel - compteur;
+
+    if (tempsEcoule < 5000) {
+        //Ou titre.style.left < window.innerWidth
+
+        document.querySelector("h2").style.left = tempsEcoule / 10 + "px";
+        // On rappelle la fonction pour boucler l'animation tant que la condition n'est pas remplie.
+        requestAnimationFrame(testAnimation);
+    }
+}
 // function onClicElement(event) {
 //     // let currentTarget = event.currentTarget; //L'élément attaché au AddEventListener
 //     let target = event.target; //Le VRAI déclencheur
